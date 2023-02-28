8228d9f58858a4a60f57077464a965c6a1bbc0e3
iamraihan <trueraihan@gmail.com>
2023-02-28 16:57:50 +0600

    folder structure added

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return <div></div>;
+};
+
+export default Navbar;
---
+++ src/routes/routes.js
@@ -0,0 +1,6 @@
+import { BrowserRouter } from "react-router-dom";
+import Home from "../pages/Home/Home";
+
+const routes = BrowserRouter([{ path: "/", element: <Home /> }]);
+
+export default routes;
