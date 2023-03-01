7605102e4ad5d5aee02c0bdea27d07b66fa30bdc
trueraihan@gmail.comgit config --global user.name trueraihan@gmail.comgit config --global user.name Raihan <trueraihan@gmail.com>
2023-03-01 23:50:20 +0600

    update

--- src/Layout/Navbar/Navbar.js
+++ src/Layout/Navbar/Navbar.js
@@ -3,8 +3,15 @@ import "./Navbar.css";
 import logo from "../../assets/images/logo ostad.png";
 import flag from "../../assets/images/english-flag.png";
 import downArrow from "../../assets/images/down-arrow.svg";
+import hamburger from "../../assets/images/companies/menu-3-line.svg";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isActive, setActive] = useState("false");
+
+  const handleToggle = () => {
+    setActive(!isActive);
+  };
   return (
     <nav className="navbar container">
       <div className="navbar-item-wrapper">
@@ -14,35 +21,40 @@ const Navbar = () => {
           </Link>
         </div>
         <div>
-          <ul className="menu-list">
-            <NavLink className="nav-link">
-              <li className="menu-item text-dark">
-                সেট ইয়োর গোল{" "}
-                <span>
-                  <img src={downArrow} alt="" />
-                </span>
-              </li>
-            </NavLink>
-            <NavLink className="nav-link">
-              <li className="menu-item text-dark">আপকামিং লাইভ ব্যাচ</li>
-            </NavLink>
-            <NavLink className="nav-link">
-              <li className="language-wrapper">
-                <img src={flag} alt={flag} />
-                <p
-                  style={{ marginLeft: "8px" }}
-                  className="menu-item text-dark"
-                >
-                  ENGLISH
-                </p>
-              </li>
-            </NavLink>
-            <NavLink className="nav-link">
-              <li>
-                <button className="btn login-btn">Login</button>
-              </li>
-            </NavLink>
-          </ul>
+          <div className={isActive ? "hidden" : null}>
+            <ul id="menus" className="menu-list ">
+              <NavLink className="nav-link">
+                <li className="menu-item text-dark">
+                  সেট ইয়োর গোল{" "}
+                  <span>
+                    <img src={downArrow} alt="" />
+                  </span>
+                </li>
+              </NavLink>
+              <NavLink className="nav-link">
+                <li className="menu-item text-dark">আপকামিং লাইভ ব্যাচ</li>
+              </NavLink>
+              <NavLink className="nav-link">
+                <li className="language-wrapper">
+                  <img src={flag} alt={flag} />
+                  <p
+                    style={{ marginLeft: "8px" }}
+                    className="menu-item text-dark"
+                  >
+                    ENGLISH
+                  </p>
+                </li>
+              </NavLink>
+              <NavLink className="nav-link">
+                <li>
+                  <button className="btn login-btn">Login</button>
+                </li>
+              </NavLink>
+            </ul>
+          </div>
+        </div>
+        <div onClick={handleToggle} className="hamburger">
+          <img src={hamburger} alt="" />
         </div>
       </div>
     </nav>
