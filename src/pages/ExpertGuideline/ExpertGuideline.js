4dcca9c2d673d990153e5e7b7cc0cebea93e50cc
iamraihan <trueraihan@gmail.com>
2023-03-01 09:50:30 +0600

    update

--- src/pages/ExpertGuideline/ExpertGuideline.js
+++ src/pages/ExpertGuideline/ExpertGuideline.js
@@ -1,9 +1,21 @@
-import React from "react";
-import "./ExpertGuideline.css";
+import React, { useRef } from "react";
 import expertBook from "../../assets/images/expert-career-guideline/book.png";
 import ExpertCareerCard from "../../components/ExpertCareerCard/ExpertCareerCard";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "./ExpertGuideline.css";
+
+// import required modules
+import { Navigation, Pagination } from "swiper";
+
 const ExpertGuideline = () => {
+  // const navigationNextRef = useRef(null);
+  // const navigationPrevRef = useRef(null);
   return (
     <div className="bg-dark">
       <div className="container expert-area-inner">
@@ -15,8 +27,73 @@ const ExpertGuideline = () => {
             এক্সপার্টদের ক্যারিয়ার গাইডলাইন
           </p>
         </div>
-        <ExpertCareerCard />
+        {/* slider  */}
+        <>
+          <Swiper
+            slidesPerView={1}
+            spaceBetween={10}
+            pagination={{
+              clickable: true,
+            }}
+            // navigation={{
+            //   nextEl: "#expert-swiper-button-next",
+            //   prevEl: "#expert-swiper-button-prev",
+            // }}
+
+            breakpoints={{
+              "@0.00": {
+                slidesPerView: 1,
+                spaceBetween: 10,
+              },
+              "@0.75": {
+                slidesPerView: 2,
+                spaceBetween: 20,
+              },
+              "@1.00": {
+                slidesPerView: 3,
+                spaceBetween: 40,
+              },
+              "@1.50": {
+                slidesPerView: 3.8,
+                spaceBetween: 50,
+              },
+            }}
+            navigation={true}
+            modules={[Pagination, Navigation]}
+            className="expertSwiper"
+          >
+            <SwiperSlide>
+              <ExpertCareerCard />
+            </SwiperSlide>
+            <SwiperSlide>
+              <ExpertCareerCard />
+            </SwiperSlide>
+            <SwiperSlide>
+              <ExpertCareerCard />
+            </SwiperSlide>
+            <SwiperSlide>
+              <ExpertCareerCard />
+            </SwiperSlide>
+            <SwiperSlide>
+              <ExpertCareerCard />
+            </SwiperSlide>
+            <SwiperSlide>
+              <ExpertCareerCard />
+            </SwiperSlide>
+            <SwiperSlide>
+              <ExpertCareerCard />
+            </SwiperSlide>
+            <SwiperSlide>
+              <ExpertCareerCard />
+            </SwiperSlide>
+            <SwiperSlide>
+              <ExpertCareerCard />
+            </SwiperSlide>
+          </Swiper>
+        </>
       </div>
+      {/* <button ref={navigationNextRef}>Hello</button>
+      <button ref={navigationPrevRef}>Prev</button> */}
     </div>
   );
 };
